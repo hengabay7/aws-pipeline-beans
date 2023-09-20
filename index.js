412ecd339ac59703ad9f7449eb396725d1e17f26
hengabay7 <hengabay7@gmail.com>
2023-09-20 19:48:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 
 app.use('/', (req,res) => {
-    return res.status(200).json({message: "Hello From AWS Pipeline"});
+    return res.status(200).json({message: "Hello From AWS Pipeline and Beanstalk"});
 })
 
 app.listen(3001);
